6bd3973e170547a664c4d7566088554f0d9deef0
Brad Busse <bbusse@gmail.com>
2016-11-07 10:46:31 -0500

    Fixes buildscreen layout

--- buildscreen.py
+++ buildscreen.py
@@ -48,39 +48,44 @@ kv = '''
         
         
                 
-<ListScreenEntry@Widget>:
-    StackLayout:
-        id: items
-        orientation: 'tb-lr'
-        padding: 5,5,5,5
+<ListScreenEntry@StackLayout>:
+    id: items
+    orientation: 'tb-lr'
+    padding: 5,5,5,5
 
     #horizontal bar
             
-    
-        canvas.before:
-            Color:
-                rgb: 0.1, 0.1, 0.1            
-            Rectangle:
-                pos:  self.pos
-                size: self.size
-                
-        canvas.after:
-            Color:
-                rgb: 0.5, 0.5, 0.5            
-            Rectangle:
-                pos:  self.pos
-                size: self.width, dp(5)     
-                                      
+
+    canvas.before:
+        Color:
+            rgb: 0.1, 0.1, 0.1            
+        Rectangle:
+            pos:  self.pos
+            size: self.size
+            
+    canvas.after:
+        Color:
+            rgb: 0.5, 0.5, 0.5            
+        Rectangle:
+            pos:  self.pos
+            size: self.width, dp(5)     
+                                  
 '''
 
 Builder.load_string(kv)
 
-class ListScreenEntry(Widget):    
+class ListScreenEntry(StackLayout):
+    verticality = NumericProperty(0)    
     def addText(self,text=''):
         newtext = Label(text=text,markup=True, size_hint= (1.0,None) )
-        self.ids['items'].add_widget(newtext)
-        self.size = (Window.width,10)
-        print self.size, self.ids['items'].minimum_size
+        newtext.height = 15
+        self.add_widget(newtext)
+
+        self.verticality += newtext.height
+        self.size = (Window.width,self.verticality)        
+        print self.size, self.minimum_size
+        
+        
 
 class BuildScreen(Screen):
     location = ListProperty([0, 0])
@@ -88,9 +93,15 @@ class BuildScreen(Screen):
     def __init__(self,**kwargs):     
         Screen.__init__(self,**kwargs) 
         self.location = kwargs['location']
+
+        hdrLbl = ListScreenEntry()
+        hdrLbl.addText('[b]TITLE OF PAGE[/b]')
+        self.ids['entries'].add_widget(hdrLbl)
         
         testLbl = ListScreenEntry()
         testLbl.addText('Test of label text')
+        testLbl.addText('Test line 2')
+        testLbl.addText('Test line 3')
         
         self.ids['entries'].add_widget(testLbl)
         
